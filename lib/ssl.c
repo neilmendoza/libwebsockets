832ad07600d9886b7a719c6fb950a3b77caf50c9
Neil Mendoza <neil@neilmendoza.com>
2019-06-04 20:58:53 -0700

    ignore warning for version used by ofxLibWebsockets

--- lib/ssl.c
+++ lib/ssl.c
@@ -18,6 +18,7 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  *  MA  02110-1301  USA
  */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #include "private-libwebsockets.h"
  #include <openssl/err.h>
